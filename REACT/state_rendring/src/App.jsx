c1fdcb3ccbdf34938ed5389055bf15121c2d0cf1
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-07-02 16:12:53 +0530

    feat: initialize state_rendring project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Included Vite logo SVG in the public directory.
    - Created main application component (App.jsx) with basic structure.
    - Implemented a Counter component (Counter.jsx) with state management.
    - Added React logo SVG in assets directory.
    - Configured Tailwind CSS in index.css.
    - Set up main entry point (main.jsx) to render the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ REACT/state_rendring/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-500 '>
+      <h1 className="text-7xl">Hello</h1>
+      <h2></h2>
+    </div>
+  )
+}
+
+export default App
