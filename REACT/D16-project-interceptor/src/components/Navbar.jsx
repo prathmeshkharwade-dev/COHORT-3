0126bfcbfe4d474ce4cf829135ffe66e9702d2a5
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-07-22 14:53:56 +0530

    feat: enhance Navbar component with active link styling for better user experience

--- REACT/D16-project-interceptor/src/components/Navbar.jsx
+++ REACT/D16-project-interceptor/src/components/Navbar.jsx
@@ -2,18 +2,40 @@ import React from 'react'
 import { NavLink } from 'react-router'
 
 const Navbar = () => {
-  return (
-    <div className='border-r border-gray-700 flex flex-col gap-10'>
-      <h1 className='text-3xl font-semibold'>E-comm</h1>
 
-      <div className='flex flex-col gap-6 ml-5'>
-        <NavLink className={"border-b border-gray-500"} to={"/main"}>Home</NavLink>
-        <NavLink className={"border-b border-gray-500"}to={"/main/users"}>Users</NavLink>
-        <NavLink className={"border-b border-gray-500"}to={"/main/products"}>Products</NavLink>
-      </div>
+    
 
-    </div>
-  )
+    return (
+        <div className='border-r border-gray-700 flex flex-col gap-10'>
+            <h1 className='text-3xl font-semibold'>E-comm</h1>
+
+            <div className='flex flex-col gap-6 ml-5'>
+                <NavLink
+                 className={({ isActive }) =>
+                    isActive
+                        ? "font-semibold text-red-600  border-b border-gray-500 "
+                        : "text-black border-b border-gray-500"
+                    }
+                    to={"/main"}
+                    end
+                    >
+                    Home
+                    </NavLink>
+
+                <NavLink className={({ isActive }) =>
+                    isActive
+                        ? "font-semibold text-red-600  border-b border-gray-500 "
+                        : "text-black border-b border-gray-500"} to={"/main/users"}>Users</NavLink>
+
+
+                <NavLink className={({ isActive }) =>
+                    isActive
+                        ? "font-semibold text-red-600  border-b border-gray-500 "
+                        : "text-black border-b border-gray-500"} to={"/main/products"}>Products</NavLink>
+            </div>
+
+        </div>
+    )
 }
 
 export default Navbar
