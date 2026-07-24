ab9c6877a7ae9e2caccf991c73d0c5ae24f12e33
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-07-24 14:36:37 +0530

    feat: add Home and About components; implement state management for count in App component

--- REACT/D17-memoization/src/App.jsx
+++ REACT/D17-memoization/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Home from './components/Home';
+import About from './components/About';
 
 const App = () => {
+
+    console.log("APP rendring.....");
+
+    const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Memoization</h1>
+      <h2>Count is {count} </h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Home />
+      <About />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ REACT/D17-memoization/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+
+      console.log("Home rendring.....");
+
+  return (
+    <div>
+      <h1>Home this side</h1>
+    </div>
+  )
+}
+
+export default Home
