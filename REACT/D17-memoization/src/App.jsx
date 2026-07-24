12e345c4f9a83d259ea772ae7139b055622183ec
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-07-24 16:51:53 +0530

    feat: add memoization for calculation in App component; display calculation result in UI

--- REACT/D17-memoization/src/App.jsx
+++ REACT/D17-memoization/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import Home from './components/Home';
 import About from './components/About';
 
@@ -8,6 +8,20 @@ const App = () => {
 
     const [count, setCount] = useState(0);
     const [users, setUsers] = useState({name: "raghav", id: 789});
+
+
+    let calculation = useMemo(() => {
+      console.log("calculation is running ...");
+      let sum = 0;
+
+      for (let i = 0; i < 10000000; i++) {
+        sum += 1;
+      }
+      return sum;
+
+    }, []);
+
+
 
     let greet = useCallback( () => {
       console.log("Good evening....");
@@ -19,6 +33,7 @@ const App = () => {
       <h1>Memoization</h1>
       <h2>Count is {count} </h2>
       <h2>Name is {users.name}</h2>
+      <h2>My Calculation is {calculation} </h2>
       <button onClick={() => setUsers({...users, name: "Ranjeet"})}>Change Name  </button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
 
