95b31a1da9d62597fde48af2ce0088534426fbf8
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-08-11 15:08:43 +0530

    feat: add array and tuple examples, enums, and union types in TypeScript

--- TypeScript/D2/index.js
+++ TypeScript/D2/index.js
@@ -18,4 +18,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // let g: bigint = 79697678n;
 //  symbol
 // let y: symbol = Symbol("hello");
+console.log("arrays tuples.");
+// let a: unknown = "faizal";
+// console.log(a.toUpperCase());
+// let y: never;
+// - Arrays
+let arr = [
+    5, 6, 7, 8, 9, 0, 67, 8, 9, 0, 6, 5, 43, 2, 3, 4, 5, 6, 7, 8, 9, 0, 87, 6, 5,
+    4, 3,
+];
+let arr1 = ["jio", "pol", "john"];
+let arrBol = [true, false, true, true, true];
+// - Tuples
+let arr2 = [56, 78, "pol", true];
+let data = [{ name: "pol" }, { name: "pol" }, { name: "pol" }];
+// enums - options
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["SUP_ADMIN"] = 1] = "SUP_ADMIN";
+    Role[Role["USER"] = 2] = "USER";
+})(Role || (Role = {}));
+let role = Role.USER;
+// union
+let yolo = "rahul";
+yolo = 90;
+let status = "pending";
 //# sourceMappingURL=index.js.map
--- TypeScript/D2/index.ts
+++ TypeScript/D2/index.ts
@@ -33,3 +33,43 @@
 
 
 
+console.log("arrays tuples.");
+
+// let a: unknown = "faizal";
+
+// console.log(a.toUpperCase());
+
+// let y: never;
+
+// - Arrays
+let arr: number[] = [
+  5, 6, 7, 8, 9, 0, 67, 8, 9, 0, 6, 5, 43, 2, 3, 4, 5, 6, 7, 8, 9, 0, 87, 6, 5,
+  4, 3,
+];
+
+let arr1: string[] = ["jio", "pol", "john"];
+
+let arrBol: boolean[] = [true, false, true, true, true];
+
+// - Tuples
+
+let arr2: [number, number, string, boolean] = [56, 78, "pol", true];
+
+let data: any[] = [{ name: "pol" }, { name: "pol" }, { name: "pol" }];
+
+// enums - options
+enum Role {
+  ADMIN,
+  SUP_ADMIN,
+  USER,
+}
+let role: Role = Role.USER;
+
+// union
+let yolo: string | number = "rahul";
+yolo = 90;
+
+// literals
+type Status = "pending" | "success" | "error";
+
+let status: Status = "pending";
