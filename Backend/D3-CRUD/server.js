4b93ca34ea5d324a36ff5af492246cf5bff13cc8
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-08-19 15:43:06 +0530

    refactor: reorganize server.js for improved readability and structure

--- Backend/D3-CRUD/server.js
+++ Backend/D3-CRUD/server.js
@@ -1,22 +1,47 @@
 const express = require("express");
 
 const app = express();
-
-
+// middleware for accepting json data
+app.use(express.json());
 let port = 3000;
 
-let user = [
-    {
-        name: "pol",
-        age: 34,
-    },
+let users = [];
 
-]
+// create
+app.post("/create", (req, res) => {
+  let body = req.body;
 
-app.get('/' ,(req, res) => {
-    res.send(user);
-})
+  users.push(body);
+
+  res.send("user saved successfully");
+});
+
+// get - Read
+app.get("/", (req, res) => {
+  res.send(users);
+});
+
+// update
+app.put("/update/:id", (req, res) => {
+  let { id } = req.params;
+  let { name } = req.body;
+
+  let updatedUser = users.map((val) =>
+    val.id === id ? { ...val, name } : val
+  );
+  res.send(updatedUser);
+});
+
+// delete
+app.delete("/delete/:id", (req, res) => {
+  let { id } = req.params;
+
+  let userData = users.filter((val) => val.id !== id);
+  console.log(userData);
+  users = userData;
+  res.send(userData);
+});
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 });
