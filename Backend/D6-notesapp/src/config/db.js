286e0d492029d605cdbcb96f4c224973cc116fc7
Prathmesh <prathmeshkharwade23@gmail.com>
2026-08-26 01:11:46 +0530

    feat: add CRUD operations for notes and integrate dotenv for environment variables

--- Backend/D6-notesapp/src/config/db.js
+++ Backend/D6-notesapp/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/notes-app");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("mongodb connected")
     } catch (error) {
         console.log("error while connecting to DB",error )
--- Backend/D6-notesapp/src/controllers/notes.controller.js
+++ Backend/D6-notesapp/src/controllers/notes.controller.js
@@ -1,40 +1,100 @@
 const NotesModel = require("../models/notes.model");
 
 const createNotesController = async (req, res) => {
-    try {
-        let { title, discription } = req.body;
+  try {
+    let { title, discription } = req.body;
 
-        let newNote = await NotesModel.create({
-            title,
-            discription,
-        });
+    let newNote = await NotesModel.create({
+      title,
+      discription,
+    });
 
-        return res.status(201).json({
-            message: "Note create Scussfully",
-            data: newNote,
-        })
-        
-    } catch (error) {
-        console.log("error in creation", error);
-    }
+    return res.status(201).json({
+      message: "Note create Scussfully",
+      data: newNote,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
 
-const getAllNotesController = async (req,res) => {
-    try {
+const getAllNotesController = async (req, res) => {
+  try {
+    const allNotes = await NotesModel.find();
 
-        const allNotes = await NotesModel.find();
-
-        res.status(200).json({
-            message: "all notes fedtched",
-            data: allNotes,
+    res.status(200).json({
+      message: "all notes fedtched",
+      data: allNotes,
     });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const getSingleNoteController = async (req, res) => {
+  try {
+    let noteId = req.params.id;
+
+    let note = await NotesModel.findById(noteId);
+
+    res.status(200).json({
+      message: "Note fetches Scussfully",
+      data: note,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const updatedNotesController = async (req, res) => {
+  try {
+    let noteId = req.params.id;
+    let body = req.body;
+
+    let updatedNote = await NotesModel.findByIdAndUpdate(noteId, body, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      message: "Note updated Scussfully",
+      data: updatedNote,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+const deletedNoteController = async (req, res) => {
+    try {
         
+        let noteId = req.params.id;
+
+        await NotesModel.findByIdAndDelete(noteId);
+        
+        return res.status(200).json({
+            message: "Note updated Scussfully",
+        });
+        
+
     } catch (error) {
-        console.log("error in get notes api", error );
-        
+        return res.status(500).json({
+            message: "Internal server error",
+        })
     }
 }
 
 module.exports = {
-    createNotesController, getAllNotesController,
+  createNotesController,
+  getAllNotesController,
+  getSingleNoteController,
+  updatedNotesController,
+  deletedNoteController,
 };
