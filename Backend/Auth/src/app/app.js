6fd9d6325f4d65113cdd3c3489e3382bda23ad14
Prathmesh <prathmeshkharwade23@gmail.com>
2026-09-02 14:49:39 +0530

    feat: initialize authentication service with Express
    - Added package.json to manage dependencies and scripts
    - Created app.js to set up Express server and define a welcome route
    - Implemented server.js to start the application on port 3000

---
+++ Backend/Auth/src/app/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.statusCode(200).json({
+        message: "Welcome to Authentication Api"
+    })
+})
+
+
+export default app;
