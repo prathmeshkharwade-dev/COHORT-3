372f5dd442cce9696c2972706574b1918461b5fa
Prathmesh <prathmeshkharwade23@gmail.com>
2026-09-04 19:10:11 +0530

    feat: create user model with schema definition for name, email, and passwordHash

---
+++ Backend/Refresh-Access-Token/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLength: 3,
+        maxLength:50
+
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    passwordHash: {
+        type: String,
+        require: true,
+        
+
+    }
+})
+
+const userModel = mongoose.model("users", userSchema)
+
+export default userModel
