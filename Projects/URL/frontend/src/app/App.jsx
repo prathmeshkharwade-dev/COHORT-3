c2bdce0e8c5f845b794f370da39457f4f703a4d2
Prathmesh <prathmeshkharwade23@gmail.com>
2026-09-18 14:29:00 +0530

    feat: refactor App component for improved URL handling and UI updates

--- Projects/URL/frontend/src/app/App.jsx
+++ Projects/URL/frontend/src/app/App.jsx
@@ -1,81 +1,112 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import './App.css'
-
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const dummyUrls = [
   {
-    _id:"1",
-    originalUrl: "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
+    _id: "1",
+    originalUrl:
+      "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
     shortCode: "SDFDD",
-    Clicks:9
+    Clicks: 9,
   },
   {
-    _id:"2",
-    originalUrl: "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
+    _id: "2",
+    originalUrl:
+      "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
     shortCode: "SDFDD",
-    Clicks:5
+    Clicks: 5,
   },
   {
-    _id:"3",
-    originalUrl: "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
+    _id: "3",
+    originalUrl:
+      "https://www.amazon.com/Ultimate-Controller-Joysticks-Remappable-Gaming-Console/dp/B0D739FJLG/ref=sr_1_24?_encoding=UTF8&sr=8-24",
     shortCode: "SDFDD",
-    Clicks:4
-  }
-]
-
-
+    Clicks: 4,
+  },
+];
 
 function App() {
+  const [urls, setUrls] = useState(dummyUrls);
+  const [inputValue, setInputValue] = useState("");
+  const [currentUrl, setCurrentUrl] = useState(null);
 
-  const [ urls, setUrls ] = useState(dummyUrls)
-  const [ inputValue, setInputValue ] = useState("")
-  const [ currentUrl, setCurrentUrl ] = useState(null)
+  async function fetchUrls() {
+    const response = await axios.get("http://localhost:5173/api/url");
 
+    const responseData = response.data;
 
-  async function fetchUrls(){
+    setUrls(responseData.data.urls);
+  }
 
-   const response = await axios.get("http://localhost:5173/api/url") 
+  async function createShortUrl() {
+    const response = await axios.post("http://localhost:5173/api/url", {
+      url: inputValue,
+    });
+    setCurrentUrl({
+      originalUrl: response.data.data.orignalUrl,
+      shortCode: response.data.data.shortCode,
+    })
+    fetchUrls()
+  }
 
-   const responseData = response.data
+  async function deleteUrl(id){
+    await axios.delete(`http://localhost:5173/api/url/${id}`)
 
-   setUrls(responseData.data.urls)
-}
+    fetchUrls()
 
-async function createShortUrl(){
-  const response = await axios.post("http://localhost:5173/api/url")
-}
+  }
 
-useEffect(() => {
-  fetchUrls()
-}, [])
+  useEffect(() => {
+    fetchUrls();
+  }, []);
 
   return (
-    <main className='p-10 flex flex-col gap-4'>
-      <div className='w-full max-w-4xl p-2 '></div>
-      <div className='w-full max-w-4xl p-2 '></div>
-      <div className='w-full max-w-4xl p-2  flex flex-col gap-2'></div>
-      {
-        urls.map(url =>{
-          return (
-            <div className='border border- bg-neutral-200 p-2 flex gap-4 justify-evenly items-center' >
-              <a href={`http://localhost:3000/${url.shortCode}`} target='_blank' >{url.shortCode}</a>
-              <p className='truncate'>{url.originalUrl}</p>
-              <p>{url.clicks}</p>
-              <div className='flex gap-2'>
-                <button className='p-2 rounded bg-amber-600 text-white cursor-pointer'>COPY</button>
-                <button className='p-2 rounded bg-amber-600 text-white cursor-pointer'>DELETE</button>
-              </div>
+    <main className="p-10 flex flex-col gap-4">
+      <div className="w-full max-w-4xl p-2 flex gap-2 ">
+        <input
+          type="text"
+          placeholder="Enter Long URL"
+          value={inputValue}
+          className="border rounded w-full p-2  "
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+        />
+        <button
+          className=" rounded p-2 bg-amber-600 text-white cursor-pointer"
+          onClick={createShortUrl}
+        >
+          shorten
+        </button>
+      </div>
+      <div className="w-full max-w-4xl p-2 "></div>
+      <div className="w-full max-w-4xl p-2  flex flex-col gap-2"></div>
+      {urls.map((url) => {
+        return (
+          <div className="border border- bg-neutral-200 p-2 flex gap-4 justify-evenly items-center">
+            <a href={`http://localhost:3000/${url.shortCode}`} target="_blank">
+              {url.shortCode}
+            </a>
+            <p className="truncate">{url.originalUrl}</p>
+            <p>{url.clicks}</p>
+            <div className="flex gap-2">
+              <button
+               className="p-2 rounded bg-amber-600 text-white cursor-pointer">
+                COPY
+              </button>
 
+              <button
+               onClick={ () => deleteUrl(url._id)}
+               className="p-2 rounded bg-amber-600 text-white cursor-pointer">
+                DELETE
+              </button>
             </div>
-          )
-        })
-      }
+          </div>
+        );
+      })}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
