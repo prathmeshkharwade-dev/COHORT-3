158e38f6f1a3361ad6949490f6010c3eaea3bfcf
Prathmesh <prathmeshkharwade23@gmail.com>
2026-09-23 01:47:05 +0530

    feat: enhance product validation with detailed checks for title, description, and price

--- Projects/snitch/server/src/validators/product.validator.js
+++ Projects/snitch/server/src/validators/product.validator.js
@@ -1,10 +1,34 @@
-import { body } from "express-validator"
+import { body } from "express-validator";
 
 export const createProductValidator = [
-    body("title")
-    .exists().withMessage("Title is required").bail()
-    .isString().withMessage("Title is must be a string").bail()
+  body("title")
+    .exists()
+    .withMessage("Title is required")
+    .bail()
+    .isString()
+    .withMessage("Title is must be a string")
+    .bail()
     .trim()
-    .isLength({ min: 2, max: 100 }).withMessage("Title length must be between 2 to 100 characters")
-    .isAlpha()
-]
+    .isLength({ min: 2, max: 100 })
+    .withMessage("Title length must be between 2 to 100 characters")
+    .bail()
+    .isAlpha("en-US", { ignore: " " })
+    .withMessage("Title can only english samll case nd upper case character"),
+  body("description")
+    .exists()
+    .withMessage("Description is required")
+    .bail()
+    .isString()
+    .withMessage("Description must be String")
+    .bail()
+    .trim()
+    .isLength({ min: 20, max: 500 })
+    .withMessage("Description length must be between 20 to 500 characters"),
+    body("price.amount")
+    .exists().withMessage("price amount is required").bail()
+    .isFloat({ min: 0 }).withMessage('Price amount must be a floating number and must be greater then zero'),
+    body("price.currency")
+    .exists().withMessage("Currency is required")
+    .isString().withMessage("Currency must be a string value")
+    .isIn([ 'INR', "USD" ]).withMessage("Currency either be INR or USD")
+];
