43f2dccbb703f634d857f11059208e182b9cc46b
Prathmesh <prathmeshkharwade23@gmail.com>
2026-09-24 16:28:22 +0530

    feat: enhance product creation with file upload handling and improved validation

--- Projects/snitch/server/src/controller/product.controller.js
+++ Projects/snitch/server/src/controller/product.controller.js
@@ -3,6 +3,7 @@ import productModel from "../models/product.model.js"
 
 export async function createProduct(req, res) {
     console.log(req.body)
+    console.log(req.files)
 
     res.status(200).json({
         message:"Dummy response"
--- Projects/snitch/server/src/routes/products.routes.js
+++ Projects/snitch/server/src/routes/products.routes.js
@@ -5,26 +5,47 @@ import { createProduct } from "../controller/product.controller.js";
 
 import multer from "multer"
 
-const upload = multer({storage: multer.memoryStorage()  })
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits:{
+        files:5,
+        fileSize: 1 * 1024 * 1024 // 1MB
+    },
+    // filefilter:- to accept files  of specific type like image,audio, video,pdf, etc[image/jpeg,image/png].
+    
+})
 
 
 const router = Router()
-
 /**
- * @method product
+ * @method POST
  * @route /api/products/
- * @description creates the product an save its data into the DB, images will be store on imagekit.
+ * @description creates the product and save its data into the DB, images will be store on imagekit.
  * @access seller
- *  req.body => { DataTransferItemList,desc:price: {amount,currency},size:[{size,stock},{size,stock}]} 
+ * req.body=>{title,description:price:{amount,currency},sizes:[{size,stock},{si–ze,stock}]}
  */
-router.post("/", authenticate, (req , res, next) => {
-    if(req.user.role !== "seller") {
-        return res.status(403).json({
-            message: "user is not authorixe to create products"
-        })
-    }
-    next()
-}, upload.array("images"), createProduct)
+router.post("/",
+    // –––––––––––––– check is user authenticate ––––––––––––––––––––
+    authenticate,
+    // –––––––––––––– check the role is seller or not ––––––––––––––––––––
+    (req, res, next) => {
+        if (req.user.role !== "seller") {
+            return res.status(403).json({
+                message: "user is not authorize to create products"
+            })
+        }
+        next()
+    },
+    // –––––––––––––– required for reading the data from req.body if the formate is form-data(multipart-form-data) ––––––––––––––––––––
+    upload.array("images"),
+    // –––––––––––––– parse the complex data like object and array into json ––––––––––––––––––––
+    (req, res, next) => {
+        req.body?.price && (req.body.price = JSON.parse(req.body.price))
+        req.body?.sizes && (req.body.sizes = JSON.parse(req.body.sizes))
+        next()
+    },
+    createProductValidator,
+    createProduct)
 
 
 export default router
